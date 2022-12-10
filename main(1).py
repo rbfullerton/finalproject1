70b2700832500cdc0ad917e0ac6a8ded276ad889
rbfullerton <118573789+rbfullerton@users.noreply.github.com>
2022-12-09 19:56:46 -0600

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,14 @@
+from selection import *
+
+
+def main():
+    app = QApplication([])
+    window = Shapes()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+
+# made by Nia and Riley CS II
